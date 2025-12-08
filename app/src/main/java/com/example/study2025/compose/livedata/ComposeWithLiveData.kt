7d57bf707d8ce006bbccddf5a3c28985cc5669c5
manishakhatri <manishakhatri756@gmail.com>
2025-12-08 08:49:35 +0530

    new updates

--- app/src/main/java/com/example/study2025/compose/livedata/ComposeWithLiveData.kt
+++ app/src/main/java/com/example/study2025/compose/livedata/ComposeWithLiveData.kt
@@ -1,4 +1,26 @@
 package com.example.study2025.compose.livedata
 
-class ComposeWithLiveData {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class LiveDataVM: ViewModel() {
+    private val _count = MutableLiveData(0)
+    val count: LiveData<Int> = _count
+
+    fun increment() {
+        _count.value = (_count.value ?: 0) + 1
+    }
 }
+
+class StateFlowVM : ViewModel() {
+    private val _state = MutableStateFlow(DataUiState())
+    val state: StateFlow<DataUiState> = _state
+}
+
+data class DataUiState(
+    val data:String = ""
+)
+
--- ds/src/main/java/com/example/ds/stack/BackspaceStringCompare.java
+++ ds/src/main/java/com/example/ds/stack/BackspaceStringCompare.java
@@ -1,4 +1,35 @@
 package com.example.ds.stack;
 
+import java.util.Stack;
+
 public class BackspaceStringCompare {
+    static public boolean backspaceCompare(String s, String t) {
+        Stack<Character> sStack = new Stack<>();
+        Stack<Character> tStack = new Stack<>();
+
+        for(char ch: s.toCharArray()) {
+            if(ch == '#') {
+                if(!sStack.isEmpty()) sStack.pop();
+            } else {
+                sStack.push(ch);
+            }
+        }
+
+        for(char ch: t.toCharArray()) {
+            if(ch == '#') {
+                if(!tStack.isEmpty()) tStack.pop();
+            } else {
+                tStack.push(ch);
+            }
+        }
+
+        return sStack.equals(tStack);
+    }
+
+    public static void main(String[] args) {
+        String s = "y#fo##f";
+        String t = "y#f#o##f";
+
+        System.out.println(backspaceCompare(s,t));
+    }
 }
